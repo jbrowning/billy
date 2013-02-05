93b31d90e7b69c2990ca88377499b84ad68e2e0b
Jeff Browning <jeff@jkbrowning.com>
2013-02-04 23:07:10 -0600

    Beginning of logged-in menu

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,23 +1,36 @@
 require 'spec_helper'
 
 describe "Users" do
-  describe "GET '/'" do
-    it "should have a login link" do
-      visit '/'
-      page.should     have_content "Login"
-      page.should_not have_content "Logout"
+  context "when logged out" do
+    describe "GET '/'" do
+      it "should have a login link" do
+        visit '/'
+        page.should     have_content "Sign in"
+        page.should_not have_content "Sign out"
+      end
     end
   end
 
-  # describe "logging in" do
-  #   let(:user) { create(:user) }
-  #   it "should show the logout link if the user has logged in" do
+  describe "logging in" do
+    let(:user) { create(:user) }
 
-  #       visit "/"
-  #       page.should have_content "Log out"
-  #       page.should_not have_content "Log in"
-  #     end
-  #   end
-  # end
+    it "should be successful with valid credentials" do
+      visit "/"
+      find(".nav").click_link "Sign in"
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'password'
+
+      click_button "Sign In"
+
+      page.should have_content "Sign out"
+      page.should_not have_content "Sign in"
+    end
+  end
+
+  context "when logged in" do
+
+  end
+
+
 
 end
