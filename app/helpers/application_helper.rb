88d1c06dc4d15733451b076c80593363cfe8eda5
Jeff Browning <jeff@jkbrowning.com>
2013-02-09 14:49:51 -0600

    Day of month options for select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,20 @@
 module ApplicationHelper
-
   def title(title=nil)
     @title ||= (title || "Bill Manager")
+  end
+
+  def day_of_month_options(selected_value=0)
+    first_option = [ "First day of month", 0 ]
+    last_option = ["Last day of month", -1]
+
+    options = (1..31).map {|n| [n, n] }
+    options.unshift first_option
+    options << last_option
+
+    selected = options[selected_value]
+
+    logger.debug selected
+
+    options_for_select options, selected
   end
 end
