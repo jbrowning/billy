e7735b604012f55b411cd5f3965114cd014fd936
Jeff Browning <jeff@jkbrowning.com>
2013-02-27 07:45:53 -0600

    Redirect user away from home of they're logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+  before_filter :redirect_user
+  
   def home
+  end
+
+
+  private
+
+  def redirect_user
+    if current_user
+      redirect_to billing_periods_path
+    end
   end
 end
