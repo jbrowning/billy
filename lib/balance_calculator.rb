64195c3898780cf1bad9314e0934cb39490c76c3
Jeff Browning <jeff@jkbrowning.com>
2013-03-01 00:34:56 -0500

    BalanceCalculator#available_balance

--- lib/balance_calculator.rb
+++ lib/balance_calculator.rb
@@ -8,4 +8,18 @@ class BalanceCalculator
     amount = @user.balance.to_s
     "#{symbol}#{amount}" 
   end
+
+  def available_balance
+    symbol = @user.balance.symbol
+    balance = @user.balance
+    available_balance = balance - unsettled_amount
+    "#{symbol}#{available_balance}"
+  end
+
+private
+
+  def unsettled_amount
+    Money.new(@user.bills.unsettled.sum(&:amount_cents))
+  end
+
 end
--- spec/lib/balance_calculator_spec.rb
+++ spec/lib/balance_calculator_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe BalanceCalculator do
+  let(:user) { build_stubbed :user, balance_cents: 10_00 }
+  let(:calc) { BalanceCalculator.new(user) }
+  
   describe '#account_balance' do
-    let(:user) { build_stubbed :user, balance_cents: 10_00 }
-    let(:calc) { BalanceCalculator.new(user) }
-    
     it 'displays the currency with symbol' do
       calc.account_balance.should eql("$10.00")
+    end
+  end
+
+  describe '#available_balance' do
+    let!(:unsettled_bill) { create :bill, :unsettled, amount_cents: 3_00, user: user }
+
+    it 'displays the correct balance with symbol' do
+      calc.available_balance.should eql("$7.00")
     end
   end
 end
