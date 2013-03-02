a8d146ddd48773649e68088c2f9032518fdaea63
Jeff Browning <jeff@jkbrowning.com>
2013-03-02 15:12:12 -0500

    Add balance well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :get_balance
+
   def after_sign_in_path_for(resource)
     billing_periods_path
+  end
+
+  def get_balance
+    if current_user
+      @balance = BalanceCalculator.new current_user
+    end
   end
 end
