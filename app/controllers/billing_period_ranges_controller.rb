36695b89b8da28c0e4636a88576add868728a209
Jeff Browning <jeff@jkbrowning.com>
2013-03-04 22:18:06 -0600

    More fixes for billing period ranges

--- app/controllers/billing_period_ranges_controller.rb
+++ app/controllers/billing_period_ranges_controller.rb
@@ -9,7 +9,7 @@ class BillingPeriodRangesController < ApplicationController
     @billing_period_range = current_user.billing_period_ranges.build(params[:billing_period_range])
 
     if @billing_period_range.save
-      redirect_to billing_periods_path
+      redirect_to billing_period_ranges_path
     else
       flash[:error] = @billing_period_range.errors.map(&:message).join(', ')
       render :index
@@ -17,8 +17,8 @@ class BillingPeriodRangesController < ApplicationController
   end
 
   def destroy
-    billing_period = BillingPeriodRange.find(params[:id])
-    if billing_period.destroy
+    billing_period_range = BillingPeriodRange.find(params[:id])
+    if billing_period_range.destroy
       flash[:notice] = "billing period deleted"
     else
       flash[:error] = "there was a problem deleting the billing period"
