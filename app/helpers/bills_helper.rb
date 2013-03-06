9b5f5613e5f295338fa650310f0bb21200be7604
Jeff Browning <jeff@jkbrowning.com>
2013-03-05 21:03:23 -0600

    Bills table tweaks

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,4 +1,9 @@
 module BillsHelper
+
+  def row_style(bill)
+    return 'warning' if bill.paid?
+  end
+
   def bill_submit_label
     if @bill
       @bill.new_record? ? "Create bill" : "Update bill"
